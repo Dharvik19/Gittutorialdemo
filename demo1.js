8908a26b70305bb89e716fe8340e745c74b275b0
Dharvik19 <dharvik.badga22>
2022-12-06 21:35:46 +0530

    added search descriptuon

--- demo1.js
+++ demo1.js
@@ -1,23 +1,27 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //form submit event 
 form.addEventListener('submit',addItem);
 //delete event 
 itemList.addEventListener('click',removeItem);
+
+filter.addEventListener('keyup',filterItems);
 //add item 
 function addItem(e){
     e.preventDefault();
     //get input value 
     var newItem = document.getElementById('item').value;
+    var newItem2 = document.getElementById('item2').value;
     //create new li element 
-
     var li = document.createElement('li');
+    
     //add class name 
     li.className = 'list-group-item';
     //add text node with input value 
     li.appendChild(document.createTextNode(newItem));
-
+    li.appendChild(document.createTextNode("\n"+newItem2));
     itemList .appendChild(li);
     //create delete button
     var deleteButton = document.createElement('button');
@@ -34,7 +38,7 @@ function addItem(e){
     //add classes to edit button
     editButton.className = 'btn btn-sm btn-secondary float-right';
     //text node 
-    editButton.appendChild(document.createTextNode('E'));
+    editButton.appendChild(document.createTextNode('Edit'));
     // append button to li 
     li.appendChild(editButton);
     //append li to list
@@ -48,4 +52,25 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+//filter items function
+
+function filterItems(e){
+//convert text
+
+var text = e.target.value.toLowerCase();
+//get list
+var items = itemList.getElementsByTagName('li');  
+//convert to array 
+Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    var itemdesc = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text)!=-1 || itemdesc.toLowerCase().indexOf(text)!=-1){
+        item.style.display = 'block';
+    }else{
+        item.style.display = 'none';
+    }
+    
+});
 }
