9079911e7bb8bdaaa5de0b531fece75d89a73467
Dharvik19 <dharvik.badga22>
2022-12-07 13:33:41 +0530

    Building app

---
+++ main.js
@@ -0,0 +1,19 @@
+// localStorage.setItem('name','bob');
+// localStorage.removeItem('name');
+//set item: to set key value pairs 
+//localstorage.get(gets the value)
+//localStorage.removeitem(pass key) :removes the value 
+
+// sessionStorage.setItem('name','john');
+// sessionStorage.removeItem('name');
+
+// document.cookie = 'name = kyle' + new Date(2022,0,1).toUTCString();
+var form = document.getElementById('my-form');
+form.addEventListener('submit',addItem);
+function addItem(e){
+    e.preventDefault();
+    var inputName = document.getElementById('name');
+    localStorage.setItem('name',inputName.value);
+    var inputEmail = document.getElementById('email');
+    localStorage.setItem('email',inputEmail.value);
+}
